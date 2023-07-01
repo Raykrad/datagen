edc68f0edb1bcc0f65c0e6f20dc8f6b18cfb045b
Raykrad <109039576+Raykrad@users.noreply.github.com>
2023-07-01 18:28:12 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "authorization.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    Authorization user;
+    user.authorizationMenu();
+    int cases = 0;
+    while (cases != 5)
+    {
+        cout << "\nMenu" << endl;
+        cout << "Possible actions:" << endl;
+        cout << "1. Personal data; \n2. Geographical data; \n3. Organizational data; \n4. Financial data; \n5. Exit." << endl;
+        cout << "Enter the desired action number: ";
+        cin >> cases;
+        switch (cases) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Введён неправильный индекс." << endl;
+            break;
+        }
+    }
+    return 0;
+}
