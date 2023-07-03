f903cca1c750fcaf42ed55e8a7364ac95dd45f68
Илюша <137887413+Slivkov@users.noreply.github.com>
2023-07-03 20:12:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 #include "authorization.h"
 #include <Windows.h>
-
 using namespace std;
 
-int main()
-{
+int main() {
     Authorization user;
     user.authorizationMenu();
     int cases = 0;
-    while (cases != 5)
-    {
+    while (cases != 5) {
         cout << "\nMenu" << endl;
         cout << "Possible actions:" << endl;
         cout << "1. Personal data; \n2. Geographical data; \n3. Organizational data; \n4. Financial data; \n5. Exit." << endl;
         cout << "Enter the desired action number: ";
         cin >> cases;
         switch (cases) {
-        case 1:
-            
+        case 1: 
+            \\ А где?
             break;
-        case 2:
+        case 2: 
             
             break;
         case 3:
-            
+
             break;
         case 4:
             
             break;
         case 5:
+            
             break;
         default:
             cout << "Incorrect index entered." << endl;
