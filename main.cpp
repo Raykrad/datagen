8c4f0f06e9172d23e2fa2f60bf335ea5bac70e77
Raykrad <109039576+Raykrad@users.noreply.github.com>
2023-07-04 10:02:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include "authorization.h"
+#include "personal_data.h"
+#include "Organizathion.h"
+#include "GeografyDatabase.h"
+#include "Economical database.h"
 #include <Windows.h>
 using namespace std;
 
@@ -9,25 +13,34 @@ int main() {
     int cases = 0;
     while (cases != 5) {
         cout << "\nMenu" << endl;
-        cout << "Possible actions:" << endl;
-        cout << "1. Personal data; \n2. Geographical data; \n3. Organizational data; \n4. Financial data; \n5. Exit." << endl;
+        cout << "\nPossible actions:" << endl;
+        cout << "\n1. Personal data;\n \n2. Geographical data;\n \n3. Organizational data;\n \n4. Financial data;\n \n5. Exit." << endl;
         cout << "Enter the desired action number: ";
         cin >> cases;
-        switch (cases) {
-        case 1: 
-            \\ А где?
+        switch (cases) 
+        {
+        case 1:
+        {
+            runPersonal();
             break;
-        case 2: 
-            
+        }
+        case 2:
+        {
+            runGeografy();
             break;
+        }
         case 3:
-
+        {
+            Organizathions();
             break;
+        }
         case 4:
-            
+        {
+            EconomicalDate eco;
+            eco.EcoMenu();
             break;
+        }
         case 5:
-            
             break;
         default:
             cout << "Incorrect index entered." << endl;
