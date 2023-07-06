ecbbdf5c44e2816412084c810262ca92aa4521e5
Mr0Henk <137883793+Mr0Henk@users.noreply.github.com>
2023-07-06 15:27:29 +0300

    Update Economical database.h
    теперь с файлами

--- Economical database.h
+++ Economical database.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <time.h>
 #include <cstdlib>
 using namespace std;
@@ -10,17 +11,17 @@ class EconomicalDate {
 private: // функции в привате
 	vector<EconomicalDate> EcoDate;
 	string CompanyName; // название компании
-       	double revenue; // доход
-       	double expenses; // расходы
-        double profit; // прибыль
+	double revenue; // доход
+	double expenses; // расходы
+	double profit; // прибыль
 	double assets; // активы
 	double liabilities; // обязательсва/кредиты        
-  	double generateRevenue() { // функция для генерации случайного значения дохода
-            	return rand() % 10000000 + 100000; // случайное число от 100 000 до 10 100 000
-        }
+	double generateRevenue() { // функция для генерации случайного значения дохода
+		return rand() % 10000000 + 100000; // случайное число от 100 000 до 10 100 000
+	}
 
 	string founderNameGeneration() { // генератор имён основателей
-		string MassName[20] = {"Egor","Iliya","Denis","Firuz","Vadim","Don","Maso","Kevin","Lenin","Oleg","Stepan","Kiril","Kovolkov","Korolev","Grisha","Menis","Malek","Alexandr","Sofia","Bob"};
+		string MassName[20] = { "Egor","Iliya","Denis","Firuz","Vadim","Don","Maso","Kevin","Lenin","Oleg","Stepan","Kiril","Kovolkov","Korolev","Grisha","Menis","Malek","Alexandr","Sofia","Bob" };
 		string name;
 		int index = rand() % 20;
 		name = MassName[index];
@@ -28,7 +29,7 @@ private: // функции в привате
 	}
 
 	string brandNameGeneration() { // генератор брендов компании
-		string MassName[20] = {"Coconut","cipsy pola","Shnapps","Stairs","Stone","DoKa","Genacide","Flowers","Bosch","Plane","Apple","Pear","Furniture","Pen","The Sun","Keyboard","Penicillin","Needle","Pineapple","Paper"};
+		string MassName[20] = { "Coconut","cipsy pola","Shnapps","Stairs","Stone","DoKa","Genacide","Flowers","Bosch","Plane","Apple","Pear","Furniture","Pen","The Sun","Keyboard","Penicillin","Needle","Pineapple","Paper" };
 		string name;
 		int index = rand() % 20;
 		name = MassName[index];
@@ -36,15 +37,15 @@ private: // функции в привате
 	}
 
 	string abbreviationNameGeneration() { // генератор абривиатур компании
-		string MassName[20] = {"OOO","AOA","VKP(b)","AES","BRIZ","VDNH","KRSHA","WPO","GO","GOST","GPT","GPU","LOL","RTYOUH","NOZING","QWE","WWW","ABC","SSA","VASH"};
+		string MassName[20] = { "OOO","AOA","VKP(b)","AES","BRIZ","VDNH","KRSHA","WPO","GO","GOST","GPT","GPU","LOL","RTYOUH","NOZING","QWE","WWW","ABC","SSA","VASH" };
 		string name;
 		int index = rand() % 20;
 		name = MassName[index];
 		return name;
 	}
-		
+
 	string FannyNameGeneration() { // генератор смешных названий компании
-		string MassName[20] = {"Cleet Jingley-Schmidt","Schlomo Clovenhoof","Bud 'Lite' Putney","Boykisser's","HellyJelly","Wit's End","The Comic Strip","Joke Warehouse","Fun n’ Funny","Crazy Laughs","Humor Haven","Laughter Zone","Fun-Loving","Laughter Lovers","Funny you said that","Comedy Castle","Hilarious Hijinks","Laughter Emporium","Joke Masters","Playful Props"};
+		string MassName[20] = { "Cleet Jingley-Schmidt","Schlomo Clovenhoof","Bud 'Lite' Putney","Boykisser's","HellyJelly","Wit's End","The Comic Strip","Joke Warehouse","Fun n’ Funny","Crazy Laughs","Humor Haven","Laughter Zone","Fun-Loving","Laughter Lovers","Funny you said that","Comedy Castle","Hilarious Hijinks","Laughter Emporium","Joke Masters","Playful Props" };
 		string name;
 		int index = rand() % 20;
 		name = MassName[index];
@@ -53,53 +54,53 @@ private: // функции в привате
 
 	string attachmentGeneration() { // генератор приставок к названию компании
 		string attach;
-		string MassAttach[10] = {"Co.","Inc.","Corp.","Studios","Indastris","Farm","Pharmaceptic","Technology","Research center","Organization"};
+		string MassAttach[10] = { "Co.","Inc.","Corp.","Studios","Indastris","Farm","Pharmaceptic","Technology","Research center","Organization" };
 		int index = rand() % 10;
 		attach = MassAttach[index];
 		return attach;
 	}
 
-       	string generateCompanyName() { // функция для генерации случайного значения дохода
+	string generateCompanyName() { // функция для генерации случайного значения дохода
 		string name;
 		int index = rand() % 7 + 1;
 		switch (index) {
-		case 1:  
+		case 1:
 			name = founderNameGeneration();
 			break;
-		case 2:  
+		case 2:
 			name = brandNameGeneration();
 			break;
-		case 3:  
+		case 3:
 			name = abbreviationNameGeneration();
 			break;
-		case 4:  
+		case 4:
 			name = FannyNameGeneration();
 			break;
-		case 5:  
-			name = founderNameGeneration() +" & " + founderNameGeneration();
+		case 5:
+			name = founderNameGeneration() + " & " + founderNameGeneration();
 			break;
-		case 6:  
+		case 6:
 			name = founderNameGeneration() + " " + brandNameGeneration();
 			break;
-		case 7:  
+		case 7:
 			name = abbreviationNameGeneration() + " " + founderNameGeneration() + " " + brandNameGeneration();
 			break;
-	     	}
+		}
 		name = name + " " + attachmentGeneration();
-        	return name; // случайное число от 100 000 до 10 100 000
-       	}
+		return name; // случайное число от 100 000 до 10 100 000
+	}
 
-    	  double generateExpenses() { // функция для генерации случайного значения расходов
-            	return rand() % static_cast<int>(generateRevenue() * 0.8) + 1000; // случайное число от 1000 до 80% от дохода
-       	}
+	double generateExpenses() { // функция для генерации случайного значения расходов
+		return rand() % static_cast<int>(generateRevenue() * 0.8) + 1000; // случайное число от 1000 до 80% от дохода
+	}
 
-        double generateAssets() { // функция для генерации случайного значения активов
-            	return rand() % 100000000 + 500000; // случайное число от 500 000 до 100 500 000
-        }
-        
-        double generateLiabilities() { // функция для генерации случайного значения обязательств/кредитов
-            	return rand() % static_cast<int>(generateAssets() * 0.5) + 50000; // случайное число от 50 000 до 50% от активов
-        }
+	double generateAssets() { // функция для генерации случайного значения активов
+		return rand() % 100000000 + 500000; // случайное число от 500 000 до 100 500 000
+	}
+
+	double generateLiabilities() { // функция для генерации случайного значения обязательств/кредитов
+		return rand() % static_cast<int>(generateAssets() * 0.5) + 50000; // случайное число от 50 000 до 50% от активов
+	}
 
 	void Show() {
 		cout << "Name company: " << CompanyName << endl;
@@ -109,16 +110,16 @@ private: // функции в привате
 		cout << "Assets: " << assets << endl;
 		cout << "Liabilities: " << liabilities << endl;
 	}
-		
+
 	double calculateNetProfit(double revenue, double expenses) { // подсчёт чистой прибыли
-           	return revenue - expenses;
-       	}
+		return revenue - expenses;
+	}
 
 	void ShowEconomicalDate(vector<EconomicalDate>& EcoDate) {
 		string cases;
 		bool emergencyShutdown = false; // предохранитель
 		if (EcoDate.size() >= 10000) {
-			cout <<"\n This is going to be a VERY long time. Are you sure?(n/y)"<< endl;
+			cout << "\n This is going to be a VERY long time. Are you sure?(n/y)" << endl;
 			while (cin.get() != '\n');
 			getline(cin, cases);
 			if (cases == "n") {
@@ -139,10 +140,10 @@ private: // функции в привате
 			}
 		}
 		if (EcoDate.size() == 0) {
-			cout<<"\n Oh, there's nothing here!"<< endl;
+			cout << "\n Oh, there's nothing here!" << endl;
 		}
 		if (emergencyShutdown == false) {
-			for (int i = 0; i<EcoDate.size();i++) {
+			for (int i = 0; i < EcoDate.size(); i++) {
 				cout << endl;
 				cout << "#" << i << ". ";
 				EcoDate[i].Show();
@@ -157,16 +158,17 @@ private: // функции в привате
 		cout << "Enter the name of company" << endl;
 		while (cin.get() != '\n');
 		getline(cin, name);
-		cout << "Enter the revenue"<<endl;
+		cout << "Enter the revenue" << endl;
 		cin >> rev;
-		cout << "Enter the expenses"<<endl;
+		cout << "Enter the expenses" << endl;
 		cin >> exp;
-		cout << "Enter the assets"<<endl;
+		cout << "Enter the assets" << endl;
 		cin >> ass;
-		cout<<"Enter the liabilities"<<endl;
+		cout << "Enter the liabilities" << endl;
 		cin >> liab;
 		prof = calculateNetProfit(rev, exp);
 		EcoDate.push_back(EconomicalDate(name, rev, exp, prof, ass, liab));
+		ResetDate(name, rev, exp, prof, ass, liab);
 	}
 
 	void EditEconomicalDate(vector<EconomicalDate>& EcoDate) {
@@ -179,40 +181,46 @@ private: // функции в привате
 		ShowEconomicalDate(EcoDate);
 		cout << "\n Enter the number of data you want to edit" << endl;
 		cin >> cases;
-		if ((cases>= 0) && (cases < EcoDate.size())) {
-			cout <<"\n Enter new company name or 0 if you do not want to change it"<<endl;
+		if ((cases >= 0) && (cases < EcoDate.size())) {
+			cout << "\n Enter new company name or 0 if you do not want to change it" << endl;
 			while (cin.get() != '\n');
 			getline(cin, newName);
-			if (newName != "0" ) {
+			if (newName != "0") {
 				EcoDate[cases].setCompanyName(newName);
 			}
-			cout <<"\n Enter new revenue or 0 if you do not want to change it"<<endl;
-			cin>>newRev;
-			if (newRev != 0 ) {
+
+			cout << "\n Enter new revenue or 0 if you do not want to change it" << endl;
+			cin >> newRev;
+			if (newRev != 0) {
 				EcoDate[cases].setRevenue(newRev);
 			}
-			cout <<"\n Enter new expenses or 0 if you do not want to change it"<<endl;
-			cin>>newExp;
-			if (newExp != 0 ) {
+
+			cout << "\n Enter new expenses or 0 if you do not want to change it" << endl;
+			cin >> newExp;
+			if (newExp != 0) {
 				EcoDate[cases].setExpenses(newExp);
 			}
+
 			cout << "\n Enter new assets or 0 if you do not want to change it" << endl;
 			cin >> newAss;
-			if (newAss != 0 ) {
+			if (newAss != 0) {
 				EcoDate[cases].setAssets(newAss);
 			}
+
 			cout << "\n Enter new liabilities or 0 if you do not want to change it" << endl;
 			cin >> newLiab;
-			if (newLiab != 0 ) {
+			if (newLiab != 0) {
 				EcoDate[cases].setLiabilities(newLiab);
 			}
-		}    
-		else {
-        		cout << "\n No data for editing was found!" << endl;
 		}
+		else {
+			cout << "\n No data for editing was found!" << endl;
+		}
+		ResetDate(EcoDate[cases].getCompanyName(), EcoDate[cases].getRevenue(), EcoDate[cases].getExpenses(), calculateNetProfit(EcoDate[cases].getRevenue(), EcoDate[cases].getExpenses()), EcoDate[cases].getAssets(), EcoDate[cases].getLiabilities());
 	}
 
 	// Я уже час переоформляю код((((((((
+	// мне тебя жалко, чел :(
 	void DeleteEconomicalDate(vector<EconomicalDate>& EcoDate) {
 		int cases;
 		ShowEconomicalDate(EcoDate);
@@ -220,7 +228,7 @@ private: // функции в привате
 		cin >> cases;
 		if ((cases >= 0) && (cases < EcoDate.size())) {
 			EcoDate.erase(EcoDate.begin() + cases);
-			cout << "\n Data deleted successfully!"<< endl;
+			cout << "\n Data deleted successfully!" << endl;
 		}
 		else {
 			cout << "\n Oops, no deletion data found!" << endl;
@@ -233,134 +241,189 @@ private: // функции в привате
 		int n;
 		cout << "\n Enter the number of generated database objects:" << endl;
 		cin >> n;
-		for (int i = 0; i < n ; i++ ) {
+		for (int i = 0; i < n; i++) {
 			name = generateCompanyName();
-           		rev = generateRevenue();
-          		exp = generateExpenses();
-          		prof = calculateNetProfit(rev, exp);
-           		ass = generateAssets();
-           		liab = generateLiabilities();
-			EcoDate.push_back(EconomicalDate(name, rev,exp,prof,ass,liab));
-		} 
+			rev = generateRevenue();
+			exp = generateExpenses();
+			prof = calculateNetProfit(rev, exp);
+			ass = generateAssets();
+			liab = generateLiabilities();
+			EcoDate.push_back(EconomicalDate(name, rev, exp, prof, ass, liab));
+			ResetDate(name, rev, exp, prof, ass, liab);
+		}
 		if (n <= 10) {
 			cout << "\n The generation was successful! It was easy." << endl;
 		}
 		else if ((n > 10) && (n <= 100)) {
-			cout <<"\n The generation was successful! That's not fast."<< endl;
+			cout << "\n The generation was successful! That's not fast." << endl;
 		}
-		else if ((n == 1911) ||( n == 911)) {
-			cout <<"\n You made it!"<< endl;
+		else if ((n == 1911) || (n == 911)) {
+			cout << "\n You made it!" << endl;
 		}
 		else if ((n > 10) && (n <= 10000)) {
-			cout <<"\n It was hard... but we made it!"<< endl;
+			cout << "\n It was hard... but we made it!" << endl;
 		}
-		else if ( n > 10000) {
-			cout <<"\n You better NOT call the <Show> function :/"<<endl;
+		else if (n > 10000) {
+			cout << "\n You better NOT call the <Show> function :/" << endl;
 		}
-       	}
+	}
+
+	void ResetDate(string name, double rev, double exp, double pro, double ass, double lia)// переписывание базы данных в файл
+	{
+		ifstream EcoDateList;
+		EcoDateList.open("d:Economical_Data.txt", ios::app);
+		if (!EcoDateList)
+		{
+			cout << "Oups! file not open :( \n \n" << endl;
+		}
+		else
+		{
+			cout << "Eah! Good open ^w^ \n \n" << endl;
+			ClearDate();
+			SaveDate(name, rev, exp, pro, ass, lia);
+		}
+	}
+
+	void SaveDate(string name, double rev, double exp, double pro, double ass, double lia)// записывает в базу данных
+	{
+		/*ifstream EcoDateList("d:Economical_Data.txt", ios::app);
+		if (EcoDateList.is_open()) 
+		{
+			EcoDateList << name << endl;
+			EcoDateList << rev << endl;
+			EcoDateList << exp << endl;
+			EcoDateList << calculateNetProfit(rev, exp) << endl;
+			EcoDateList << ass << endl;
+			EcoDateList << lia << "\n" << endl;
+			EcoDateList.close();
+			cout << "Data saved." << endl;
+		}*/
+		string filename = "d:Economical_Data.txt";
+		ofstream EcoDateList(filename, ios::app);
+		if (EcoDateList.is_open())
+		{
+			EcoDateList << name << endl;
+			EcoDateList << rev << endl;
+			EcoDateList << exp << endl;
+			EcoDateList << calculateNetProfit(rev, exp) << endl;
+			EcoDateList << ass << endl;
+			EcoDateList << lia << "\n" << endl;
+			EcoDateList.close();
+			cout << "Data saved." << endl;
+		}
+		else {
+			cout << "File opening error." << endl;
+		}
+	}
+
+	void ClearDate()// удаляет базу данных
+	{
+		fstream clear_file("Economical_Data.txt", ios::out);
+		clear_file.close();
+	}
 
 public:
 	void EcoMenu() {
 		int cases = 0;
 		bool EcoCompleted = false;
-	       	while (cases != 6) {
-            		cout<< "\n Select the item for further work:"<<endl;
-            		cout<< "\n 1. Output financial data"<<endl;
-            		cout<< "\n 2. Generate new data N times"<<endl;
-            		cout<< "\n 3. Generate new data manually"<<endl;
-            		cout<< "\n 4. Edit existing data"<<endl;
-            		cout<< "\n 5. Delete existing data"<<endl;
-          		cout<< "\n 6. Exit"<<endl;
-           		cout<< "\n Please, enter number of your chose:"<<endl;
-           		cin >> cases;
-			
-            		switch (cases) {
-            		case 1:
-                		ShowEconomicalDate(EcoDate);
-               	 		break;
-            		case 2:
-               			FullGenerate(EcoDate);
-               	 		break;
-            		case 3:
-                		EnterEconomicalDate(EcoDate);
-                		break;
-            		case 4:
-                		EditEconomicalDate(EcoDate);
-                		break;
-            		case 5:
-                		DeleteEconomicalDate(EcoDate);
-                		break;
-            		case 6:
-                		cout<<"Goodbye!"<<endl;
-                		break;
+		while (cases != 6) {
+			cout << "\n Select the item for further work:" << endl;
+			cout << "\n 1. Output financial data" << endl;
+			cout << "\n 2. Generate new data N times" << endl;
+			cout << "\n 3. Generate new data manually" << endl;
+			cout << "\n 4. Edit existing data" << endl;
+			cout << "\n 5. Delete existing data" << endl;
+			cout << "\n 6. Exit" << endl;
+			cout << "\n Please, enter number of your chose:" << endl;
+			cin >> cases;
+
+			switch (cases) {
+			case 1:
+				ShowEconomicalDate(EcoDate);
+				break;
+			case 2:
+				FullGenerate(EcoDate);
+				break;
+			case 3:
+				EnterEconomicalDate(EcoDate);
+				break;
+			case 4:
+				EditEconomicalDate(EcoDate);
+				break;
+			case 5:
+				DeleteEconomicalDate(EcoDate);
+				break;
+			case 6:
+				cout << "Goodbye!" << endl;
+				break;
 			}
-        	}
+		}
 	}
-		
+
 	~EconomicalDate() { } // деконструктор по умолчанию
 
 	EconomicalDate() { // конструктор по умолчанию
 		srand(time(NULL));
 		CompanyName = "the company name must be here";
 		revenue = 0.0;
-	        expenses = 0.0;
-	       	profit = 0.0;
-	       	assets = 0.0;
-	        liabilities = 0.0;
-        }
-        
-        EconomicalDate(string name, double rev, double exp, double pro, double ass, double lia) { // конструктор с параметрами
+		expenses = 0.0;
+		profit = 0.0;
+		assets = 0.0;
+		liabilities = 0.0;
+	}
+
+	EconomicalDate(string name, double rev, double exp, double pro, double ass, double lia) { // конструктор с параметрами
 		CompanyName = name;
 		revenue = rev;
-	        expenses = exp;
-	        profit = pro;
-	        assets = ass;
-	        liabilities = lia;
+		expenses = exp;
+		profit = pro;
+		assets = ass;
+		liabilities = lia;
 	}
 
 	// Гетеры / сетеры
 	double getRevenue() {
-            	return revenue;
-        }
-
-        void setRevenue(double rev) {
-            	revenue = rev;
-        }
-	string getCompanyName() {
-            	return CompanyName;
-        }
-        void setCompanyName(string name) {
-            	CompanyName = name;
-       	 }
-
-        double getExpenses() {
-            	return expenses;
-        }
-        void setExpenses(double exp) {
-            	expenses = exp;
-        }
-
-        double getProfit() {
-            	return profit;
-        }
-
-        void setProfit(double pro) {
-            	profit = pro;
-        }
-
-        double getAssets() {
-            	return assets;
-        }
-
-        void setAssets(double ass) {
-        	assets = ass;
-        }
-
-        double getLiabilities() {
-        	return liabilities;
+		return revenue;
 	}
 
-        void setLiabilities(double lia) {
-        	liabilities = lia;
-        }
+	void setRevenue(double rev) {
+		revenue = rev;
+	}
+	string getCompanyName() {
+		return CompanyName;
+	}
+	void setCompanyName(string name) {
+		CompanyName = name;
+	}
+
+	double getExpenses() {
+		return expenses;
+	}
+	void setExpenses(double exp) {
+		expenses = exp;
+	}
+
+	double getProfit() {
+		return profit;
+	}
+
+	void setProfit(double pro) {
+		profit = pro;
+	}
+
+	double getAssets() {
+		return assets;
+	}
+
+	void setAssets(double ass) {
+		assets = ass;
+	}
+
+	double getLiabilities() {
+		return liabilities;
+	}
+
+	void setLiabilities(double lia) {
+		liabilities = lia;
+	}
 };
